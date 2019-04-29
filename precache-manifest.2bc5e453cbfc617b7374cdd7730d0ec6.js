6269c0a32e2a5daed48cabdf3871bcac0be0a1af
“SivonneWard” <sivonne@live.unc.edu>
2019-04-29 12:18:01 -0400

    add initial site files

---
+++ precache-manifest.2bc5e453cbfc617b7374cdd7730d0ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90eb69cbece0de653c42",
+    "url": "/simpsons-app/static/js/runtime~main.27324021.js"
+  },
+  {
+    "revision": "45672df68fbfb6ba2363",
+    "url": "/simpsons-app/static/js/main.b4ccbe84.chunk.js"
+  },
+  {
+    "revision": "7e508ce73fd2850c8c76",
+    "url": "/simpsons-app/static/js/2.0323eb27.chunk.js"
+  },
+  {
+    "revision": "45672df68fbfb6ba2363",
+    "url": "/simpsons-app/static/css/main.c174e461.chunk.css"
+  },
+  {
+    "revision": "5625d9ddb03e7156616445e811807133",
+    "url": "/simpsons-app/index.html"
+  }
+];
