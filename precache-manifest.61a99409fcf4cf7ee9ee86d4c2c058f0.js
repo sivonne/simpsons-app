a2e44cb299f2bbaf547810b1827a3e66ec7c9b20
“SivonneWard” <sivonne@live.unc.edu>
2019-04-29 22:01:12 -0400

    second deploy, updated the colophon, images, and scrollmagic elements

---
+++ precache-manifest.61a99409fcf4cf7ee9ee86d4c2c058f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90eb69cbece0de653c42",
+    "url": "/simpsons-app/static/js/runtime~main.27324021.js"
+  },
+  {
+    "revision": "c71bd146a043582c2c0d",
+    "url": "/simpsons-app/static/js/main.777e6a60.chunk.js"
+  },
+  {
+    "revision": "b712e9dbf422767349e0",
+    "url": "/simpsons-app/static/js/2.6e310887.chunk.js"
+  },
+  {
+    "revision": "c71bd146a043582c2c0d",
+    "url": "/simpsons-app/static/css/main.bc8f2685.chunk.css"
+  },
+  {
+    "revision": "976cfac87dd31a28ca8134f0855ddf3a",
+    "url": "/simpsons-app/index.html"
+  }
+];
